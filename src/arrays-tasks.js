47f3ada8c2ddddd0a405bf6dbd463a48d073cf8e
Natali Matvieiko <70928980+Nataliia-Matvieiko@users.noreply.github.com>
2024-01-12 14:33:05 +0100

    feat: solve sortDigitNamesByNumericOrder task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -546,8 +546,22 @@ function shiftArray(/* arr, n */) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const namesOfNumbers = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  return arr.sort(
+    (a, b) => namesOfNumbers.indexOf(a) - namesOfNumbers.indexOf(b)
+  );
 }
 
 /**
